114eeff35e4e80bbd263386bee19db037e956533
madhukiran03 <vvce23cse0108@vvce.ac.in>
2025-11-29 13:28:59 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,19 @@
 print("hello")
 print("vvce")
 print("college")
+"""import java.io.FileReader;
+public class ReadJSON{
+   public static void main(String[] args)throws IOException,ParseException {
+   JSONParser jsonparser=new JSONPareser();
+   FileReader reader=new FileReader(".\\JSON\\student.json");
+   Object obj=jsonparser.parse(reader);
+   JSONObject studentobj=(JSONObject)obj;
+   String fname=(String)studentobj.get("firstname");
+    String lname=(String)studentobj.get("lastname");
+
+    print("firstname:"+fname);
+     print("firstname:"+lname);
+     }
+     }"""
+    
+   
